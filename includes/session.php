80778c2af2ae418fed4609e5c060cb369cece695
TitanicThompson1 <45363817+TitanicThompson1@users.noreply.github.com>
2019-12-03 15:21:09 +0000

    Update session.php

--- includes/session.php
+++ includes/session.php
@@ -1,4 +1,18 @@
+
 <?php
+session_start();
+
+function generate_random_token() {
+  return bin2hex(openssl_random_pseudo_bytes(32));
+}
+
+if (!isset($_SESSION['csrf'])) {
+  $_SESSION['csrf'] = generate_random_token();
+}
+
+
+
+/*
 session_start();                         // starts the session
 include_once('../database/connection.php'); // connects to the database
 include_once('../database/user.php');      // loads the functions responsible for the users table
@@ -28,5 +42,6 @@ function getEmail()
         return null;
     }
 }
-
+*/
 ?>
+
