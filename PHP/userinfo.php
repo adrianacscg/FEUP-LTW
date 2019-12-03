96f0a8cea399934b827b259180f02809afd978fd
joao <joaopaulo_n@hotmail.com>
2019-12-03 16:47:19 +0000

    up

--- PHP/userinfo.php
+++ PHP/userinfo.php
@@ -1,20 +1,26 @@
 <?php
-include_once('../database/database.php');
+include_once('../includes/database.php');
 
 function getName($email)
 {
-    global $db;
+    //tenta ligar à base de dados
+    try{
+        $dbh= Database::instance()->db();
+  
+      }catch(Exception $e){
+        return $e;
+    }
+
     try {
-      $stmt = $db->prepare('SELECT nomeCompleto FROM Utilizador WHERE email = ?');
-      $stmt->execute($email);
+      $stmt = $dbh->prepare('SELECT nomeCompleto FROM Utilizador WHERE email = :email');
+      $stmt->bindParam(':email', $email);
+      $stmt->execute();
       return $stmt->fetch();
     
     }catch(PDOException $e) {
       return null;
     }
 }
-
-
 
 ?>
 
